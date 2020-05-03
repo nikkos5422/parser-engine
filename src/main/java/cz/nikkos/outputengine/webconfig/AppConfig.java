2a7b507f2ea75acbd73e012eb21dd658609b4d67
Nikolai Kosariev <kosarnik@fit.cvut.cz>
2020-05-03 21:38:20 +0200

    clean up the code

--- src/main/java/cz/nikkos/outputengine/webconfig/AppConfig.java
+++ src/main/java/cz/nikkos/outputengine/webconfig/AppConfig.java
@@ -13,16 +13,7 @@ public class AppConfig {
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.sendgrid.net");
-        mailSender.setPort(25);
-        mailSender.setUsername("apikey");
-        mailSender.setPassword("SG.K3WYiCldT-O-9-FN3AdzOw.MHo8Y0c5V87KiZ************Cb9lk");
 
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "false");
-        props.put("mail.debug", "true");
 
         return mailSender;
     }
